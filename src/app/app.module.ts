8be3bccdc971cd3af228d156bdfd2e560c7f6883
DasherSlade <nahuel4002@gmail.com>
2023-02-10 06:25:41 -0300

    avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import  {  NgCircleProgressModule  }  from  'ng-circle-progress';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    BannerComponent,
+    SobreMiComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+    HabilidadesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule. forRoot ({ })
   ],
   providers: [],
   bootstrap: [AppComponent]
