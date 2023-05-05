96064c5da086192a2e41da8848e7180b4370f02e
DasherSlade <nahuel4002@gmail.com>
2023-05-04 23:08:59 -0300

    actualizacion de imagenes

--- src/app/components/educacion/neweducacion.component.ts
+++ src/app/components/educacion/neweducacion.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Educacion } from 'src/app/model/educacion';
 import { EducacionService } from 'src/app/service/educacion.service';
+import { ImageService } from 'src/app/service/image.service';
 
 @Component({
   selector: 'app-neweducacion',
@@ -9,17 +10,23 @@ import { EducacionService } from 'src/app/service/educacion.service';
   styleUrls: ['./neweducacion.component.css']
 })
 export class NeweducacionComponent implements OnInit {
+  cargandoImagen: boolean = false;
   nombreEdu: string;
   descripcionEdu: string;
   img: string;
 
-  constructor(private educacionS: EducacionService, private router: Router) { }
+  constructor(
+    private educacionS: EducacionService, 
+    private router: Router,
+    public imageService: ImageService
+    ) { }
 
   ngOnInit(): void {
   }
 
   onCreate(): void {
     const educacion = new Educacion(this.nombreEdu, this.descripcionEdu, this.img);
+    educacion.img = this.imageService.url;
     this.educacionS.save(educacion).subscribe(
       data => {
         alert("Educacion añadida correctamente");
@@ -29,5 +36,15 @@ export class NeweducacionComponent implements OnInit {
         this.router.navigate(['']);
       }
     )
+  }
+
+  uploadImage($event: any) {
+    const file = $event.target.files[0];
+    const name = 'educacion_' + Date.now();
+    this.cargandoImagen = true;
+    this.imageService.uploadImage(file, name).then(() => {
+      this.cargandoImagen = false;
+      this.img = this.imageService.url;
+    });
   }
 }
